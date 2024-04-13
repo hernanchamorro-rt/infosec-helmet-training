5bffbc0b0e1b9c72d56e785eca874b66c63af69b
Hernan Chamorro <hernan.chamorro@real-trends.com>
2024-04-13 17:10:00 -0300

    Configure helmet using the parent helmet() middleware

--- myApp.js
+++ myApp.js
@@ -2,11 +2,34 @@ const helmet = require("helmet");
 const express = require("express");
 const app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: "deny" }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: "deny" }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, force: true }));
+// app.use(helmet.dnsPrefetchControl());
+
+// This are not included if you use app.use(helmet());
+// app.use(helmet.noCache());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", "trusted-cdn.com"],
+//     },
+//   })
+// );
+
+app.use(
+  helmet({
+    frameguard: { action: "deny" },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-cdn.com"],
+      },
+    },
+    dnsPrefetchControl: false,
+  })
+);
